7d94a6324e1d55f1474ef09d2e375d4b88121cee
germansola <solagerman00@gmail.com>
2021-10-17 17:10:17 -0300

    first files

---
+++ app.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from fastapi import Depends, HTTPException, status
+from database import *
+
+app = FastAPI(
+    title = "mystery"
+)
+
+origins = [
+    "http://localhost:3000"
+    "localhost:3000"
+]

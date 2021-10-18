46428e2f560d45db730f0eeb57a6c1e9e6bec91d
germansola <solagerman00@gmail.com>
2021-10-17 22:28:59 -0300

    implementacion creacion de partida

--- database.py
+++ database.py
@@ -2,5 +2,34 @@ from pony.orm import *
 
 db = pony.orm.Database()
 
-db.bind('mysql', host='127.0.0.1', user='root', passwd='', db='mystery')
+db.bind("mysql", host="127.0.0.1", user="root", passwd="", db="mystery")
 
+# Games table and Functions
+
+
+class Player(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    nickname = Required(str)
+    host = Required(bool)
+    game = Required("Game")
+
+
+class Game(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    name = Required(str)
+    is_started = Required(bool)
+    is_full = Required(bool)
+    num_players = Optional(int)
+    players = Set(Player)
+
+
+db.generate_mapping(create_tables=True)
+
+@db_session
+def new_game(new_name):
+    Game(name=new_name, is_started=False, is_full=False)
+
+@db_session
+def game_exist(un_name):
+    if Game.get(name=un_name) is not None:
+        return True

22c510bc192a866c75a8062ba0ea8c4d3200fb3a
AgustinCapogrossi <agus_capo_19@hotmail.com>
2021-10-19 01:47:45 -0300

    LOS-47

--- app.py
+++ app.py
@@ -2,11 +2,6 @@ from fastapi import FastAPI
 from fastapi import Depends, HTTPException, status
 from database import *
 
-app = FastAPI(
-    title = "mystery"
-)
+app = FastAPI(title="mystery")
 
-origins = [
-    "http://localhost:3000"
-    "localhost:3000"
-]
+origins = ["http://localhost:3000" "localhost:3000"]
--- database.py
+++ database.py
@@ -2,5 +2,4 @@ from pony.orm import *
 
 db = pony.orm.Database()
 
-db.bind('mysql', host='127.0.0.1', user='root', passwd='', db='mystery')
-
+db.bind("mysql", host="127.0.0.1", user="root", passwd="", db="mystery")

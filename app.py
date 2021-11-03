dbb2723b8a7c15d24ca14270cf17bb04924635d5
AgustinCapogrossi <agus_capo_19@hotmail.com>
2021-11-02 22:35:14 -0300

    Websocket

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
 from fastapi import Depends, HTTPException, status
+from typing import List
+from fastapi.responses import HTMLResponse
 from database import *
 from pydantic_models import *
 from fastapi.middleware.cors import CORSMiddleware
@@ -28,6 +30,30 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+# ----------------------------------------- WEBSOCKET -----------------------------------------
+class ConnectionManager:
+    def __init__(self):
+        self.connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.connections.append(websocket)
+
+    async def broadcast(self, data: str):
+        for connection in self.connections:
+            await connection.send_text(data)
+
+
+manager = ConnectionManager()
+
+
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    while True:
+        data = await websocket.receive_text()
+        await manager.broadcast(f"Client {client_id}: {data}")
+
 
 # ----------------------------------------- USER -----------------------------------------
 
